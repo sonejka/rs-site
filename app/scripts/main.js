bdc2c8934577a6224fa08db8cc208da470cde46a
kseniya-varabei <kseniya.varabei@gmail.com>
2014-09-22 17:06:49 +0300

    created mobile version of site

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+window.onload = function(){
+    var el = document.getElementById("nav");
+    var navBtn = document.getElementById("nav-btn");
+
+    window.toggleClass = function() {
+        if (el.className == "vertical-mode-closed") {
+            el.className = "vertical-mode";
+            navBtn.innerHTML = 'x';
+        } else {
+            el.className = "vertical-mode-closed";
+            navBtn.innerHTML = '&#x2261;';
+        }
+    }
+
+    window.onresize = function () {
+        el.className = "vertical-mode-closed";
+        navBtn.innerHTML = '&#x2261;';
+    }
+
+}
